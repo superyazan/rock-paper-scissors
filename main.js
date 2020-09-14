31fe8af302af665462dd81397436b093466272a6
Yazan <elyazanmuhamed@gmail.com>
2020-09-14 13:51:38 +0200

    console played

---
+++ main.js
@@ -0,0 +1,76 @@
+let pScore = 0;
+let cScore = 0;
+const game = () => {
+    const choices = ['rock','paper','scissors'];
+    const cnumbers = Math.floor(Math.random() * 3);
+    let computerChoice = choices[cnumbers];
+
+
+let playerchoice = prompt('What is your choice?');
+let pchoice = playerchoice.toLocaleLowerCase();
+let round = () => {
+    if (pchoice === computerChoice){
+        console.log('It is a tie , you get the same');
+    }
+    else {
+        if (pchoice === 'rock'){
+            if (computerChoice === 'scissors'){
+                console.log('u win, rock beats scissors');
+                pScore++
+                console.log(`your score is ${pScore} vs ${cScore} for computer`)
+
+            }
+            else {
+                console.log('u lose, rocks never beat paper')
+                cScore++
+                console.log(`your score is ${pScore} vs ${cScore} for computer`)
+
+            }
+            
+        }
+        else if (pchoice === 'paper'){
+            if (computerChoice === 'scissors'){
+                console.log('u lose, paper never beat scissors');
+                cScore++
+                console.log(`your score is ${pScore} vs ${cScore} for computer`)
+
+            }
+            else {
+                console.log(' u win, paper beats rock');
+                pScore++
+                console.log(`your score is ${pScore} vs ${cScore} for computer`)
+
+            }
+        }
+        else if (pchoice === 'scissors'){
+            if (computerChoice === 'rock'){
+                console.log('u lose, scissors never beat rock');
+                cScore++
+                console.log(`your score is ${pScore} vs ${cScore} for computer`)
+
+            }
+            else {
+                console.log('u win, scissors beats paper')
+                pScore++
+                console.log(`your score is ${pScore} vs ${cScore} for computer`)
+            }
+        }
+        else {
+            console.log('choose only between rock, scissors and paper. and pay attention to misstyping.');
+        }
+
+
+        }
+}
+    
+
+    
+round();
+ 
+}
+ 
+game();
+game();
+game();
+game();
+game();
